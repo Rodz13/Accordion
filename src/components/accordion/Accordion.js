37877e626f17f9ddeb969ded768ab319ae42fe3a
Rodrigo <rodrigomirapalheta@hotmail.com>
2023-01-14 16:06:01 +0200

    Add main title colors when all tasks are selected

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -7,13 +7,20 @@ import styles from '../../../styles/Accordion.module.css';
 
 const Accordion = ({ title, content }) => {
 	const [isActive, setIsActive] = useState(false);
+	const [isAllSelected, setIsAllSelected] = useState(false);
+
+	const handleSelectionChange = (isAllSelected) => {
+		setIsAllSelected(isAllSelected);
+	};
+
+	const titleColor = isAllSelected ? '#00B797' : '#333333';
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content} onClick={() => setIsActive(!isActive)}>
 				<div className={styles.title}>
 					<BsClipboardData className={styles.icon}/>
-					<div>{title}</div>
+					<div style={{ color :titleColor }}>{title}</div>
 				</div>
 				<div className={styles.toggle}>
 					<div>{isActive ? 'Hide' : 'Show'}</div>
@@ -24,8 +31,9 @@ const Accordion = ({ title, content }) => {
 			</div>
 			{isActive && content.map((c, index) => (
 				<Tasks 
-					description={c.description}
+					description={c.description || c.name}
 					key={index}
+					onSelectionChange={handleSelectionChange}
 				/>
 			))}
 		</div>
--- src/components/accordion/Tasks.js
+++ src/components/accordion/Tasks.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '../../../styles/Accordion.module.css';
 
-const Tasks = ({ description }) => {
+const Tasks = ({ description, onSelectionChange }) => {
+	const [selected, setSelected] = useState([]);
+
+	const handleChange = (e) => {
+		if (e.target.checked) {
+		  setSelected([...selected, e.target.value]);
+		} else {
+		  setSelected(selected.filter((item) => item !== e.target.value));
+		}
+		onSelectionChange(selected.length === description.length)
+	  };
+
 	return (
 		<div className={styles.wrapper}>
 			<label className={styles.label}>
-				<input className={styles.input} type="checkbox"/>
+				<input 
+				className={styles.input}
+				type="checkbox"
+				value={description}
+				checked={selected.includes(description)}
+				onChange={handleChange}
+			/>
 				{description}
 			</label>
 		</div>
